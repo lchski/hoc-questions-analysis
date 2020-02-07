210ff9e25747e4a357f1c8174f2881bd21ce5cf5
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-02-06 23:22:49 -0500

    everything's more interesting in COMPARATIVE PERSPECTIVE!

--- analyze.R
+++ analyze.R
@@ -5,4 +5,7 @@ questions_by_parliament %>%
   group_by(parliament, session, question_number) %>%
   mutate(time_until_response = response_date - question_date) %>%
   ungroup() %>%
+  group_by(parliament, session) %>%
   skim(time_until_response)
+
+
