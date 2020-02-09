72bf820cd99459aa0e7936ed3d44c431bdf782a4
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-02-09 00:08:24 -0500

    clean up question details bit

--- scripts/load-question-details-for-each-parliament.R
+++ scripts/load-question-details-for-each-parliament.R
@@ -1,9 +1,5 @@
 source("scripts/load-question-details.R")
 
-
-## get a sense of how many requests per session (aka how many sitting days there were with Qs asked)
-questions_by_parliament %>% count_group(parliament, session, question_sitting_day) %>% count()
-
 get_detailed_questions_for_parliamentary_session <- function(parliament_to_check, session_to_check, fast = FALSE) {
   if (fast) {
     scraper = scrape_questions_content_for_day
@@ -20,16 +16,20 @@ get_detailed_questions_for_parliamentary_session <- function(parliament_to_check
     unnest(cols = c(detailed_questions), names_repair = "universal")
 }
 
+## get a sense of how many requests per session (aka how many sitting days there were with Qs asked)
+questions_by_parliament %>% count_group(parliament, session, question_sitting_day) %>% count()
+
 p40_1_questions <- get_detailed_questions_for_parliamentary_session(40, 1, fast = TRUE)
+p40_1_questions %>% write_csv("data/out/detailed-questions/40-1.csv")
 
-p43_1_questions <- questions_by_parliament %>%
-  filter(parliament == 43) %>%
-  distinct(parliament, session, question_sitting_day) %>%
-  mutate(
-    detailed_questions = pmap(., slow_scrape_questions_content_for_day)
-  ) %>%
-  unnest(cols = c(detailed_questions))
+p43_1_questions <- get_detailed_questions_for_parliamentary_session(43, 1, fast = TRUE)
+p43_1_questions %>% write_csv("data/out/detailed-questions/43-1.csv")
 
+## combine all the detailed question objects, left join them to the question index
 questions_by_parliament %>%
-  left_join(p43_questions, by = c("parliament", "session", "question_number", "asker_name", "asker_riding"))
+  left_join(
+    p40_1_questions %>%
+      rbind(p43_1_questions),
+    by = c("parliament", "session", "question_number", "asker_name", "asker_riding")
+  )
 
