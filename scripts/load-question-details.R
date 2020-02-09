9bfd1ac232533954ddec8232c3073f00300bbb58
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-02-08 21:52:29 -0500

    solidify queries even more (yay xpath!)

--- scripts/load-question-details.R
+++ scripts/load-question-details.R
@@ -13,21 +13,20 @@ scrape_questions_content_for_day <- function(parliament, session, sitting_day) {
     ))
   
   object_ids <- notice_paper %>%
-    html_nodes(xpath = '//td[@class="JustifiedTop ItemPara"]//b/..') %>%
+    html_nodes(xpath = '//td[@class="JustifiedTop ItemPara"]//b[contains(text(), "Q-")]/..') %>%
     html_node("b") %>%
     html_nodes(xpath="./text()[normalize-space()]") %>%
     html_text(trim=TRUE)
   
   detailed_questions <- notice_paper %>%
-    html_nodes(xpath = '//td[@class="JustifiedTop ItemPara"]//b/..') %>%
+    html_nodes(xpath = '//td[@class="JustifiedTop ItemPara"]//b[contains(text(), "Q-")]/..') %>%
     html_text() %>% 
-    str_split(" — ") %>%
+    str_split(" — ", n = 4) %>%
     transpose() %>%
     as_tibble(.name_repair = ~ c("question_number", "question_date", "person", "question_content")) %>%
     unnest(cols = c(question_number, question_date, person, question_content)) %>%
     mutate(question_number = object_ids) %>%
     mutate(question_date = mdy(question_date)) %>%
-    filter(str_detect(question_number, "Q-")) %>% ## getting rid of M- etc.
     mutate(question_number = as.integer(str_remove(question_number, "Q-"))) %>%
     separate(person, into = c("asker_name", "asker_riding"), " \\(") %>%
     mutate(asker_riding = str_remove(asker_riding, "\\)"))
