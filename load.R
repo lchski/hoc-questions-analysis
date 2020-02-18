245f81193747426bde0e29f50879bd958230575f
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-02-17 22:02:46 -0500

    output combined file

--- load.R
+++ load.R
@@ -111,3 +111,7 @@ questions_and_responses <- questions_by_parliament %>%
       response_content
     )
   )
+
+questions_and_responses %>%
+  unnest(c(responses)) %>%
+  write_csv("data/out/questions_and_responses.csv")
